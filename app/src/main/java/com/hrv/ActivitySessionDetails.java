2b85d574b4c32fd3ee407a59d98a31172a46c89a
manishautomatic <manishautomatic@gmail.com>
2016-09-19 19:45:02 +0530

    committing BLE scanner and depending classes

---
+++ app/src/main/java/com/hrv/ActivitySessionDetails.java
@@ -0,0 +1,18 @@
+package com.hrv;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by manishautomatic on 19/09/16.
+ */
+
+public class ActivitySessionDetails extends AppCompatActivity {
+
+
+    @Override
+    public void onCreate(Bundle bundle){
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_session_details);
+    }
+}
---
+++ app/src/main/java/com/hrv/ActivitySessionsHistory.java
@@ -0,0 +1,19 @@
+package com.hrv;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by manishautomatic on 19/09/16.
+ */
+
+public class ActivitySessionsHistory extends AppCompatActivity {
+
+
+    @Override
+    public void onCreate(Bundle bundle){
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_sessions_history);
+
+    }
+}
